5da127fe9ddf4fa60fd6dd04bb0c30d60842cd27
Maryna Beskorovainaya <iam.marik@gmail.com>
2015-06-22 16:03:17 +0300

    move settings to server variables

--- settings.py
+++ settings.py
@@ -6,10 +6,10 @@ POST_PER_PAGE = 9
 
 DB_URL = os.environ.get('DATABASE_URL')
 
-MAIL_USERNAME = ''
-MAIL_PASSWORD = ''
-FLASKY_MAIL_SENDER = ''
-SECRET_KEY = ''
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+FLASKY_MAIL_SENDER = os.environ.get('FLASKY_MAIL_SENDER')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 try:
     from local_settings import *
 except ImportError:
